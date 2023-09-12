b4ae3f8b95006a664a110807c87b17bc841660fb
WashingtonCarrillo <52978075+WashingtonCarrillo@users.noreply.github.com>
2023-09-12 01:24:41 -0500

    Add files via upload

---
+++ src/main/java/com/desarrollo/centralconfiguration/CentralConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.desarrollo.centralconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigurationApplication.class, args);
+	}
+
+}
